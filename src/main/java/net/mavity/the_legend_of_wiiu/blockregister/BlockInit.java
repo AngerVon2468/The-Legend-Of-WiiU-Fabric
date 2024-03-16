a6510ed81525af8d047527fcd4efe98c95be6545
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-16 16:58:43 +1100

    Added a spot to make blocks
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

---
+++ src/main/java/net/mavity/the_legend_of_wiiu/blockregister/BlockInit.java
@@ -0,0 +1,31 @@
+package net.mavity.the_legend_of_wiiu.blockregister;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.mavity.the_legend_of_wiiu.TheLegendOfWiiU;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class BlockInit {
+    //testing to see if I can register a block or not
+    public static final Block EXAMPLE_BLOCK = registerBlock("example_block",
+            new Block(FabricBlockSettings.create().noCollision().notSolid()));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(TheLegendOfWiiU.MOD_ID, name), block);
+    }
+
+    public static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(TheLegendOfWiiU.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().maxCount(64)));
+    }
+
+    public static void registerBlockInit() {
+        TheLegendOfWiiU.LOGGER.info("Registering Blocks for TLOW (" + TheLegendOfWiiU.MOD_ID + ")");
+    }
+}
