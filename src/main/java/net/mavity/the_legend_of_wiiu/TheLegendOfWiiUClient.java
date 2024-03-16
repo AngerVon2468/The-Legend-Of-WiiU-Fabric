c26d7ae827d5fe9ccb6b338480f582081bca8505
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-16 12:54:53 +1100

    Added Client stuff
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

---
+++ src/main/java/net/mavity/the_legend_of_wiiu/TheLegendOfWiiUClient.java
@@ -0,0 +1,10 @@
+package net.mavity.the_legend_of_wiiu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheLegendOfWiiUClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
