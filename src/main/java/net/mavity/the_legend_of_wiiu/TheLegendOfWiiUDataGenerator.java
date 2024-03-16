7056468745821b426a2e752baf2692adcfb40f76
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-16 11:58:03 +1100

    Base commit

---
+++ src/main/java/net/mavity/the_legend_of_wiiu/TheLegendOfWiiUDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mavity.the_legend_of_wiiu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheLegendOfWiiUDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
