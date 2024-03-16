9d5610b6ba94b775befca5ac8118f3817cc2b6c3
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-16 19:29:47 +1100

    Ported all items from NeoForge
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/itemregister/ItemInit.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/itemregister/ItemInit.java
@@ -1,11 +1,11 @@
 package net.mavity.the_legend_of_wiiu.itemregister;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.mavity.the_legend_of_wiiu.TheLegendOfWiiU;
 import net.mavity.the_legend_of_wiiu.propertymanager.TLOWProperties;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -24,6 +24,11 @@ public class ItemInit {
         entries.add(SILVER_RUPEE);
         entries.add(GOLD_RUPEE);
     }
+    private static void addCombatTabMethod(FabricItemGroupEntries entries) {
+        entries.add(ROYAL_GUARDS_SWORD);
+        entries.add(THE_MASTER_SWORD);
+        entries.add(THE_AWAKENED_MASTER_SWORD);
+    }
 
     public static final Item GREEN_RUPEE = registerItem("green_rupee", new Item(TLOWProperties.RUPEETYPE));
     public static final Item BLUE_RUPEE = registerItem("blue_rupee", new Item(TLOWProperties.RUPEETYPE));
@@ -32,10 +37,18 @@ public class ItemInit {
     public static final Item PURPLE_RUPEE = registerItem("purple_rupee", new Item(TLOWProperties.RUPEETYPE));
     public static final Item SILVER_RUPEE = registerItem("silver_rupee", new Item(TLOWProperties.RUPEETYPE));
     public static final Item GOLD_RUPEE = registerItem("gold_rupee", new Item(TLOWProperties.RUPEETYPE));
-
+    public static final Item THE_WIIU = registerItem("wiiu", new Item(TLOWProperties.RUPEETYPE));
+    public static final Item ROYAL_GUARDS_SWORD = registerItem("royal_guards_sword",
+            new SwordItem(ToolMaterials.WOOD, 7, -2.4f, new FabricItemSettings().maxDamage(64)));
+    public static final Item THE_MASTER_SWORD = registerItem("the_master_sword",
+            new SwordItem(ToolMaterials.DIAMOND, 8, -2.4f, new FabricItemSettings().maxDamage(552)));
+    public static final Item THE_AWAKENED_MASTER_SWORD = registerItem("the_awakened_master_sword",
+            new SwordItem(ToolMaterials.DIAMOND, 14, -2.4f, new FabricItemSettings().maxDamage(1024)));
+    public static final Item MAJORAS_MASK = registerItem("majoras_mask", new Item(new FabricItemSettings().maxDamage(0).maxCount(1)));
     public static void registerItemInit() {
         TheLegendOfWiiU.LOGGER.info("Registering Items for TLOW (" + TheLegendOfWiiU.MOD_ID + ")");
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ItemInit::addToTabMethod);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ItemInit::addCombatTabMethod);
     }
 }
