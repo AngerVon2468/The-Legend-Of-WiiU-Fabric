5f776be749a769802f0670b851d18dce3365a600
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-16 14:11:35 +1100

    Made properties (Also I somehow lost it in the commit but I registered all rupees too)
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

---
+++ src/main/java/net/mavity/the_legend_of_wiiu/propertymanager/TLOWProperties.java
@@ -0,0 +1,7 @@
+package net.mavity.the_legend_of_wiiu.propertymanager;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+
+public class TLOWProperties {
+    public static final FabricItemSettings RUPEETYPE = new FabricItemSettings().maxCount(64).fireproof();
+}
