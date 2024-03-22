e62b01c0edbda8d0dc00a4770c29fc50c9fb395c
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-22 11:32:47 +1100

    BREAK THE POT!!!!
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/blockregister/blocktyperegister/PotType.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/blockregister/blocktyperegister/PotType.java
@@ -4,17 +4,21 @@ import net.mavity.the_legend_of_wiiu.TheLegendOfWiiU;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class PotType extends HorizontalFacingBlock {
     public PotType(Settings settings) {
         super(settings);
     }
-
-    //note to future me: figure out the custom voxelshape for fabric and put it here
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         VoxelShape shape = VoxelShapes.empty();
@@ -27,6 +31,14 @@ public class PotType extends HorizontalFacingBlock {
         shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.3125, 0.8125, 0.3125, 0.6875, 0.9375, 0.6875));
 
         return shape;
+    }
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(Text.literal("You know you want to... just... BREAK THE POT ALREADY!!!"), false);
+        }
+
+        return ActionResult.SUCCESS;
     }
     //Leave this at the bottom just in case
     public static void registerPotType() {
