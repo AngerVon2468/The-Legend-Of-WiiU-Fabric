abf80a323914f2733d17a5acf8f2db0eed5d4b85
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-22 11:11:04 +1100

    Example code for future me
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

---
+++ src/main/java/net/mavity/the_legend_of_wiiu/blockregister/blocktyperegister/examplevoxelshapes.java
@@ -0,0 +1,20 @@
+package net.mavity.the_legend_of_wiiu.blockregister.blocktyperegister;
+
+public class examplevoxelshapes {
+    //This is so IntelliJ stops having a seizure about this.
+}
+
+//An example of the code you need for custom shape, everything else normally here is irrelevant.
+
+/*
+	VoxelShape shape = VoxelShapes.empty();
+	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0, 0.1875, 0.8125, 0.6875, 0.8125));
+	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.6875, 0.4375, 0.5625, 0.8125, 0.5625));
+	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.375, 0.6875, 0.4375, 0.4375, 0.8125, 0.5625));
+	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.5625, 0.6875, 0.4375, 0.625, 0.8125, 0.5625));
+	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.6875, 0.5625, 0.5625, 0.8125, 0.625));
+	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.6875, 0.375, 0.5625, 0.8125, 0.4375));
+	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.3125, 0.8125, 0.3125, 0.6875, 0.9375, 0.6875));
+
+	return shape;
+*/
