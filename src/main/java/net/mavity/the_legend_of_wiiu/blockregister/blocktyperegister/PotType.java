f09a06cfc5228b2b18ad52f7bbd0869a72b4ada4
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-22 11:10:54 +1100

    Added custom VoxelShape to Pot
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/blockregister/blocktyperegister/PotType.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/blockregister/blocktyperegister/PotType.java
@@ -1,7 +1,13 @@
 package net.mavity.the_legend_of_wiiu.blockregister.blocktyperegister;
 
 import net.mavity.the_legend_of_wiiu.TheLegendOfWiiU;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class PotType extends HorizontalFacingBlock {
     public PotType(Settings settings) {
@@ -9,7 +15,19 @@ public class PotType extends HorizontalFacingBlock {
     }
 
     //note to future me: figure out the custom voxelshape for fabric and put it here
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        VoxelShape shape = VoxelShapes.empty();
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0, 0.1875, 0.8125, 0.6875, 0.8125));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.6875, 0.4375, 0.5625, 0.8125, 0.5625));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.375, 0.6875, 0.4375, 0.4375, 0.8125, 0.5625));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.5625, 0.6875, 0.4375, 0.625, 0.8125, 0.5625));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.6875, 0.5625, 0.5625, 0.8125, 0.625));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.6875, 0.375, 0.5625, 0.8125, 0.4375));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.3125, 0.8125, 0.3125, 0.6875, 0.9375, 0.6875));
 
+        return shape;
+    }
     //Leave this at the bottom just in case
     public static void registerPotType() {
         TheLegendOfWiiU.LOGGER.info(TheLegendOfWiiU.MOD_ID + " has registered a blocktype named PotType.");
